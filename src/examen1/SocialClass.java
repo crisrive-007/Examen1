e178e83ebb46eb369443e05d3618fce9c23c0729
camilaflores07 <camila_maria@unitec.edu>
2025-02-21 13:47:19 -0600

    clase socialclass terminada

--- src/examen1/SocialClass.java
+++ src/examen1/SocialClass.java
@@ -5,19 +5,49 @@
 package examen1;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author 50494
  */
-public class SocialClass {
-    ArrayList <String> friends = new ArrayList<>();
-    ArrayList <String> username = new ArrayList<>();
+public abstract class SocialClass {
+    private String username;
+    protected ArrayList <String> friends = new ArrayList<>();
+    protected ArrayList <String> posts = new ArrayList<>();
 
     public SocialClass(String username) {
+        this.username = username;
         this.friends = new ArrayList();
-        this.username = new ArrayList();
+        this.posts = new ArrayList();
     }
-
-   
+    
+     public boolean addFriend(String user) {
+        if (user == null || user.isEmpty() || user.equals(username) || friends.contains(user)) {
+            return false;
+        }
+        friends.add(user);
+        return true;
+    }
+    
+    public void addPost(String msg){
+       if (msg != null && !msg.isEmpty()) {
+            posts.add(msg);
+        }
+    }
+    
+    public abstract void timeline();
+    
+    public void myProfile(){
+        String profileInfo = "Usuario: " + username + "\n\n";   
+        timeline(); 
+        profileInfo += "Amigos:\n";
+        for (int i = 0; i < friends.size(); i++) {
+            profileInfo += friends.get(i) + " ";
+            if ((i + 1) % 10 == 0) {
+                profileInfo += "\n"; 
+            }
+        }
+        JOptionPane.showMessageDialog(null, profileInfo, "Perfil de " + username, JOptionPane.INFORMATION_MESSAGE);
+    }
 }
